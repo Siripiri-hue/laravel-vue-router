ff1817a5c02a4d0d87ab2bf98a27fcc3e48b2b01
Siripiri-hue <daemonprocellae@live.it>
2022-05-14 12:20:03 +0200

    Creata rotta api

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,7 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+// rotta collegata all'api PostController
+// tutte le rotte avranno in automatico il prefisso api
+Route::get('/posts', 'Api\PostController@index');
